b5af559ee7e70545252268a2bc70648d470c2d1f
FahmidaMahjabin <mehjabineva.ce@gmail.com>
2022-05-24 22:55:05 +0600

    connect to database

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_username}:${process.env.DB_password}@cluster0.dfsqs.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    await client.connect();
+    const partsCollection = client.db("manufacturer-website").collection("manufacturer-parts");
+    try{
+    
+  // perform actions on the collection object
+    console.log("mongodb connected")
+    app.get("/purchase", async(req, res) =>{
+        res.send("This is purchase")
+    })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
